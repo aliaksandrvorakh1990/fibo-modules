d1338917af2299aecfa6f1eaef1e6c98cdaafad3
avorakh <avorakh@netgear.com>
2020-06-19 19:51:46 +0300

    [FIBO-1] add application class

---
+++ task-receiver/src/main/java/by/vorakh/dev/fibo/Application.java
@@ -0,0 +1,13 @@
+package by.vorakh.dev.fibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
