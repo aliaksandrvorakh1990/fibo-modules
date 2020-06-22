14832db90f0d761bc2cb2903e71d4be9cc779c8b
avorakh <avorakh@netgear.com>
2020-06-22 14:35:35 +0300

    [FIBO-1] fix the 'getResult' method to return null if a result is not exist

--- fibo-counter/src/main/java/by/vorakh/dev/fibo/fibo_counter/repository/impl/JdbcTaskResultRepository.java
+++ fibo-counter/src/main/java/by/vorakh/dev/fibo/fibo_counter/repository/impl/JdbcTaskResultRepository.java
@@ -2,19 +2,23 @@ package by.vorakh.dev.fibo.fibo_counter.repository.impl;
 
 import by.vorakh.dev.fibo.fibo_counter.repository.TaskResultRepository;
 import by.vorakh.dev.fibo.fibo_counter.repository.entity.TaskResultEntity;
+import by.vorakh.dev.fibo.fibo_counter.repository.impl.rowmapper.TaskResultEntityRowMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.PreparedStatement;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
+@Log
 @RequiredArgsConstructor
 public class JdbcTaskResultRepository implements TaskResultRepository {
 
     private final static String WRITE_RESULT = "INSERT INTO task_results (task_result_id, result) VALUES (?, ?)";
 
-    private final static String GET_RESULT_BY_ID = "SELECT result FROM task_results WHERE task_result_id = ?";
+    private final static String GET_RESULT_BY_ID = "SELECT * FROM task_results WHERE task_result_id = ?";
 
     private final JdbcTemplate jdbcTemplate;
     private final Executor repositoryExecutor;
@@ -40,7 +44,18 @@ public class JdbcTaskResultRepository implements TaskResultRepository {
     public CompletableFuture<String> getResultBy(long taskId) {
 
         return CompletableFuture.supplyAsync(
-            () -> jdbcTemplate.queryForObject(GET_RESULT_BY_ID, String.class, taskId),
+            () -> {
+                List<TaskResultEntity> listWithAResult = jdbcTemplate.query(
+                    connection -> {
+                        PreparedStatement ps = connection.prepareStatement(GET_RESULT_BY_ID);
+                        ps.setLong(1, taskId);
+                        return ps;
+                    },
+                    new TaskResultEntityRowMapper()
+                );
+                String result = (listWithAResult.isEmpty()) ? null : listWithAResult.get(0).getResult();
+                return  result;
+            },
             repositoryExecutor
         );
     }
