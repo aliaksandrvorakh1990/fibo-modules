eaa422fe67f90afe73b42f7603940df7d2972597
avorakh <avorakh@netgear.com>
2020-06-22 17:05:24 +0300

    [FIBO-1] add no arg constructor for SolvedTaskViewModel

--- task-receiver/src/main/java/by/vorakh/dev/fibo/task_receiver/model/response/SolvedTaskViewModel.java
+++ task-receiver/src/main/java/by/vorakh/dev/fibo/task_receiver/model/response/SolvedTaskViewModel.java
@@ -2,9 +2,11 @@ package by.vorakh.dev.fibo.task_receiver.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SolvedTaskViewModel {
 
     private String result;
