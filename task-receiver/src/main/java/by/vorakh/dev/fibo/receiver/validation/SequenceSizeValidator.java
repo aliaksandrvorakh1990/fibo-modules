a0218b495cad0e72081b1e79b134e2b70ea703bd
avorakh <avorakh@netgear.com>
2020-06-26 16:42:58 +0300

    [FIBO-2] add Not null annotations for Sequence Size Validator

--- task-receiver/src/main/java/by/vorakh/dev/fibo/receiver/validation/SequenceSizeValidator.java
+++ task-receiver/src/main/java/by/vorakh/dev/fibo/receiver/validation/SequenceSizeValidator.java
@@ -1,8 +1,9 @@
-package by.vorakh.dev.fibo.task_receiver.validation;
+package by.vorakh.dev.fibo.receiver.validation;
 
-import by.vorakh.dev.fibo.task_receiver.model.payload.SequenceSize;
+import by.vorakh.dev.fibo.receiver.model.payload.SequenceSize;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
@@ -12,7 +13,7 @@ public class SequenceSizeValidator {
     private final static int LEFT_BORDER = 0;
     private final static int RIGHT_BORDER = 2000;
 
-    public static boolean isCorrectSize(SequenceSize sequenceSize) {
+    public static boolean isCorrectSize(@NotNull SequenceSize sequenceSize) {
 
         return Optional.ofNullable(sequenceSize)
             .map(SequenceSize::getSize)
