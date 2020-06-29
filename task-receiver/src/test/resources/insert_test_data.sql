4b90413e636771d2aa9d002e5a2e0def9b32c948
avorakh <avorakh@netgear.com>
2020-06-29 16:25:33 +0300

    [FIBO-2] fix inserting scripts for  new time fields for tests

--- task-receiver/src/test/resources/insert_test_data.sql
+++ task-receiver/src/test/resources/insert_test_data.sql
@@ -1,5 +1,11 @@
-INSERT INTO tasks(`task_id`, `number`, `isCompleted`, `startProcessing`, `finishProcessing`) VALUES (1, 2, TRUE, '2020-06-22 13:08:13', '2020-06-22 13:09:00');
+INSERT INTO tasks(`task_id`, `number`, `status`, `creationTime`, `endTime`, `result` )
+VALUES (1, 2, 'COMPLETED', 1593024253765, 1593025749413, '0, 1');
 
-INSERT INTO task_results(task_result_id, result) VALUES (1, '0, 1');
+INSERT INTO tasks(`task_id`, `number`, `status`, `creationTime`)
+VALUES (2, 4, 'PROCESSING', 1593024256765);
 
-INSERT INTO tasks(`task_id`, `number`, `isCompleted`, `startProcessing`) VALUES (2, 200, FALSE , '2020-06-22 13:02:13');
+INSERT INTO tasks(`task_id`, `number`, `status`, `creationTime`)
+VALUES (3, 5, 'FAILED', 1593024753765);
+
+INSERT INTO tasks(`task_id`, `number`, `status`, `creationTime`)
+VALUES (4, 15, 'CREATED', 1593024953765);
