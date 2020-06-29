f7e9224a804e976d3934a7bff10f710cffa7a627
avorakh <avorakh@netgear.com>
2020-06-29 16:23:02 +0300

    [FIBO-2] fix migration scripts for  new time fields

--- fibo-counter/src/main/resources/migration/R__create_fibo_schema.sql
+++ fibo-counter/src/main/resources/migration/R__create_fibo_schema.sql
@@ -2,9 +2,8 @@ CREATE TABLE IF NOT EXISTS `tasks` (
     `task_id` BIGINT AUTO_INCREMENT,
     `number` INT NOT NULL,
     `status` VARCHAR (15) NOT NULL,
-    `startProcessing` BIGINT NOT NULL,
-    `finishProcessing` BIGINT,
+    `creationTime` BIGINT NOT NULL,
+    `endTime` BIGINT,
     `result` TEXT,
     CONSTRAINT task_pk PRIMARY KEY (task_id)
 );
-
--- task-receiver/src/test/resources/migration/R__create_test_schema.sql
+++ task-receiver/src/test/resources/migration/R__create_test_schema.sql
@@ -1,14 +1,9 @@
 CREATE TABLE IF NOT EXISTS `tasks` (
     `task_id` BIGINT AUTO_INCREMENT,
     `number` INT NOT NULL,
-    `isCompleted` BOOLEAN,
-    `startProcessing` TIMESTAMP NOT NULL,
-    `finishProcessing` TIMESTAMP,
+    `status` VARCHAR (15) NOT NULL,
+    `creationTime` BIGINT NOT NULL,
+    `endTime` BIGINT,
+    `result` TEXT,
     CONSTRAINT task_pk PRIMARY KEY (task_id)
-);
-
-CREATE TABLE If NOT EXISTS `task_results` (
-    `task_result_id` BIGINT UNIQUE NOT NULL ,
-    `result` TEXT NOT NULL,
-     CONSTRAINT rlt_task_fk FOREIGN KEY (task_result_id) REFERENCES tasks (task_id)
 );
