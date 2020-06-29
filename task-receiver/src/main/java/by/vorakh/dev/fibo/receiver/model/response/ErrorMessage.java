dc979c2b70cd349955570a7eedd77aa2c0d8725b
avorakh <avorakh@netgear.com>
2020-06-29 16:39:59 +0300

    [FIBO-2] fix using lombok

--- task-receiver/src/main/java/by/vorakh/dev/fibo/receiver/model/response/ErrorMessage.java
+++ task-receiver/src/main/java/by/vorakh/dev/fibo/receiver/model/response/ErrorMessage.java
@@ -1,10 +1,8 @@
 package by.vorakh.dev.fibo.receiver.model.response;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
 public class ErrorMessage {
 
     private final String message;
--- task-receiver/src/main/java/by/vorakh/dev/fibo/receiver/model/response/ResultResponse.java
+++ task-receiver/src/main/java/by/vorakh/dev/fibo/receiver/model/response/ResultResponse.java
@@ -1,13 +1,10 @@
 package by.vorakh.dev.fibo.receiver.model.response;
 
 import by.vorakh.dev.fibo.counter.repository.entity.TaskStatus;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-public class TaskResponse {
+public class ResultResponse {
 
     private final TaskStatus status;
     private final String result;
